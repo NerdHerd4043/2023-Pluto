2f735fe57f2a1ae747973d25095510f4377d2bdb
JJW-Math <99464519+JJW-Math@users.noreply.github.com>
2023-01-28 18:36:02 -0800

    Updated AprilTag auto
    It now drives until the AprilTag is the correct distance away. It's inconsistent.

--- src/main/java/frc/robot/commands/autonomous/DistanceDrive.java
+++ src/main/java/frc/robot/commands/autonomous/DistanceDrive.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.autonomous;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
@@ -26,7 +27,7 @@ public class DistanceDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.drive(0.3, 0);
+    drivetrain.drive(-0.3, 0);
   }
 
   // Called once the command ends or is interrupted.
@@ -39,6 +40,6 @@ public class DistanceDrive extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return SmartDashboard.getNumber("limelight/ta", 0) == 0.26;
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0) >= 0.425;
   }
 }
